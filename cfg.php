4b81670d2d83cc8a2b894cf0302dc22b2ece59b6
Hilarius Prin Pujianto <prinpujianto@yahoo.com>
2021-01-25 12:08:12 +0700

    added hp

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+//BAHAN YANG PERLU DIINSTAL DI TERMUX
+//[1]. pkg install php
+//[2]. pkg install tesseract
+//[3]. pkg install imagemagick
+//Sampai Sini sudah Paham Kan 
+//Jangan Lupa Subscribe Chanel Penghasil Gratisa
+
+
+$ua = "Mozilla/5.0 (Linux; Android 10; POCOPHONE F1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+
+$PHPSESSID = "PHPSESSID=flg0c7mn5sm9dtrkpf8l849kl4";
+
+$get_id = "get_id=132010";

a276b02fd93b90a9781bec1efaeb81aa4ea7c57c
Hilarius Prin Pujianto <prinpujianto@yahoo.com>
2021-01-29 12:16:37 +0700

    auto

--- cfg.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-$useragent = "Mozilla/5.0 (Linux; Android 10; POCOPHONE F1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36";
-
-$cookie = "PHPSESSID=nes6dekd05898alcdjj9sd7hu3";
-
-

2d69f95acaa2e7c6ad11f60bb3e6b373af8e180c
Hilarius Prin Pujianto <prinpujianto@yahoo.com>
2021-02-09 16:01:12 +0700

    fiv2

---
+++ cfg.php
@@ -0,0 +1,6 @@
+<?php
+
+$ua = "Mozilla/5.0 (Linux; Android 10; POCOPHONE F1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
+$session = "df8d7aed-7a8b-407b-ae68-c7d27e736abb.-WOhcqgXP6P_Qo6jy_G63b2pIR8";
+$__cfduid = "d1ba3da8151b3fd49447cd022430101321612856977";
+$data = "&coins=doge&coins=trx&coins=ltc&coins=btc";

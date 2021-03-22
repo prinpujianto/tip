b302c6c113e5ec6598b1f9ea35d796ba238854d5
Hilarius Prin Pujianto <prinpujianto@yahoo.com>
2021-03-22 21:44:55 +0700

    waher

--- cfg.php
+++ cfg.php
@@ -1,21 +1,12 @@
 <?php
 
-
-# Your User-Agent 👇
-
+// Your User-Agent
 $uag = "Mozilla/5.0 (Linux; Android 10; POCOPHONE F1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
 
 
-
-# Your Cookie 👇
-
-$cook = '_cfduid=dedeaa779c2c9f96f32d3fc5ddf3e06b21615950819;cf_clearance=637a892eb56c8586e24fc2565d463d330cf22b6c-1615950883-0-250;hy=j04snj73baqdg38h5bvh8gr16d;ip_check_time=ip_check_time;_gid=GA1.2.235078143.1615959381;bitmedia_fid=eyJmaWQiOiIxYjNjYzM0MjdmMGE0YTVlZmFlYTk5NWRmYTY2MWM1MSIsImZpZG5vdWEiOiIxOThhMDBiNWYwNDY5NzgwNzgwYTRjYmFmMTIxMWM3ZiJ9;cf_bm=908dafcd7e860314c0fb7be6664e10308be70a24-1615959383-1800-AZ/gNeCFQkQNDuO6430jtdgOK2lHeNDyDx/DS5z3GS5S71WwYQ/QZdxns+0DCQzWPzem+CsgNcNiWMQvO5j52/QkMEZamyIDr4bzybr1XLHwrl11wP2q/6Zfz3uubMlz4g==;bidswitch_last_time=1615959503765;_viCookieActive=true;rekmob_props_608696=%7B%22date%22%3A1615959453264%2C%22rekJs%22%3A%7B%22rekmob_ad_unit_type%22%3A3%2C%22rekmob_native_type%22%3Anull%2C%22rekmob_ad_width%22%3A728%2C%22rekmob_fixed_cpm%22%3A0%2C%22rekmob_network_ids%22%3A%22anx_placement_id%3D18125040%3Badf_placement_id%3D750436%3Bcrt_id%3D1%22%2C%22rekmob_ad_unit%22%3A%22ca1d4e92ad744076b117c4fcc7f06411%22%2C%22rekmob_app_type%22%3A0%2C%22rekmob_ad_height%22%3A90%2C%22region_id%22%3A608696%7D%2C%22countryCode%22%3A%22TH%22%2C%22cookieTime%22%3A1615959505870%7D;rekmob_last_seen_ca1d4e92ad744076b117c4fcc7f06411=1615959507430;rekmob_props_550889=%7B%22date%22%3A1615959350098%2C%22rekJs%22%3A%7B%22rekmob_ad_unit_type%22%3A23%2C%22rekmob_native_type%22%3Anull%2C%22rekmob_ad_width%22%3A160%2C%22rekmob_fixed_cpm%22%3A0%2C%22rekmob_network_ids%22%3A%22anx_placement_id%3D16163571%3Badf_placement_id%3D678913%3Bimdi_placement_id%3D22042051%3Bcrt_id%3D1%22%2C%22rekmob_ad_unit%22%3A%22c46d0a5e50364255877c9cb30a454ea3%22%2C%22rekmob_app_type%22%3A0%2C%22rekmob_ad_height%22%3A600%2C%22region_id%22%3A550889%7D%2C%22countryCode%22%3A%22TH%22%2C%22cookieTime%22%3A1615959510282%7D;_data_html=110-1;_ga=GA1.2.1195386178.1615959380;cto_bundle=xLA8J19QQzlaNlREQmZzSzN5JTJCN2Y0ejVmRnpaOVk1Y0M2RGdoMldDNjklMkZJdUJodkxScTBLa0ZScDB1Y0JCdTFkZ1VXbzdGRWlMbUxRTW80VFRDTVZCUUVWRllwJTJCOVJ5ZU1yOFBHZmx4M2VUJTJCUmwwQ0FVM1JFbm5UV2k2TlBTdUwyUSUyQlloRlNvNVdlVTRiMkU3VzN5MFpOUXhBJTNEJTNE;rekmob_last_seen_c46d0a5e50364255877c9cb30a454ea3=1615959581396;_ga_6P782LQ7C9=GS1.1.1615959379.1.1.1615959637.0;_gat_gtag_UA_26960785_2=1;_gat_gtag_UA_26960785_4=1';
+// Your Cookie
+$cook = 'humans_21909=1;_cfduid=d0cdb795d893a4b0a5e3376c606b1ddb71616423513;PHPSESSID=2c7fd0b5e3c5ee49f6e21c6ac88b7ba7;_ga=GA1.2.269295439.1616423531;_gid=GA1.2.726616936.1616423531;_gads=ID=5cbdaf6a5764679c-22157ea449b9006c:T=1616423532:RT=1616423532:S=ALNI_Maxqz5pr61bzmiWP1RQd2q2DIMTyw;adbit-viewed-ads=20157;csrf_cookie_name=c21ecbf241ca574b396b8d9e85dcb9d1;ci_session=22d86eae085226bb37c21d492b8330ead68e7368';
 
 
-
-# On For Visit Ptc Off For AutoFaucet
-
-$ptc = "On";
-
-
-?>
+// Timer On Website
+$time= "80";

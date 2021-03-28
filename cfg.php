cd0e0fcdfe0e64282cb762cbef4c4006ebd5be66
Hilarius Prin Pujianto <prinpujianto@yahoo.com>
2021-03-28 20:09:45 +0700

    star

--- cfg.php
+++ cfg.php
@@ -1,12 +1,19 @@
 <?php
 
-// Your User-Agent
+
+# Your User-Agent 👇
+
 $uag = "Mozilla/5.0 (Linux; Android 10; POCOPHONE F1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.152 Mobile Safari/537.36";
 
 
-// Your Cookie
-$cook = 'humans_21909=1;_cfduid=d0cdb795d893a4b0a5e3376c606b1ddb71616423513;PHPSESSID=2c7fd0b5e3c5ee49f6e21c6ac88b7ba7;_ga=GA1.2.269295439.1616423531;_gid=GA1.2.726616936.1616423531;_gads=ID=5cbdaf6a5764679c-22157ea449b9006c:T=1616423532:RT=1616423532:S=ALNI_Maxqz5pr61bzmiWP1RQd2q2DIMTyw;adbit-viewed-ads=20157;csrf_cookie_name=c21ecbf241ca574b396b8d9e85dcb9d1;ci_session=22d86eae085226bb37c21d492b8330ead68e7368';
+
+# Your Cookie 👇
+
+$cook = "DOGEToken=Z4Q7ibo2FGlrIY1G6egeNnrHwMdcvVfG;__cfduid=d94b43cbf266572e586726a2aaaf9ce7f1616936711;PHPSESSID=otvg4b93ug0mj8jfl7e0s1hcij";
 
 
-// Timer On Website
-$time= "80";
+
+
+
+
+?>
